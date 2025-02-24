8986797724444dc0022a8e72bc90d89d8e62ea48
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2025-02-24 22:41:31 +0000

    added test text on screen

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Iot.Device.Graphics;
+using Iot.Device.LEDMatrix;
+
+PinMapping mapping = PinMapping.MatrixBonnetMapping32;
+
+RGBLedMatrix matrix = new RGBLedMatrix(mapping, 64, 32);
+matrix.StartRendering();
+
+// matrix.FillRectangle(0, 0, 10, 10, 255, 0, 0);
 
 
+BdfFont font = BdfFont.Load(@"fonts/4x6.bdf");
 
-Console.WriteLine("Hello, World!");
+matrix.DrawText(0,1,"Hello World", font, 255, 255, 255, 0, 0, 0);
+matrix.DrawText(0,10,"London Liv Street", font, 255, 255, 255, 0, 0, 0);
+
+
+while(true)
+{
+    Thread.Sleep(1000);
+
+}
