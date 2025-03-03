948026bb90c4556b16716fba837950953f3070c7
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-03-03 07:21:10 +0000

    created function to scroll text across the screen

--- TrainBoard/Program.cs
+++ TrainBoard/Program.cs
@@ -27,14 +27,36 @@ Console.CancelKeyPress += (_, e) =>
     running = false;
     e.Cancel = true;
 };
+
+int scrollTextPos = canvas.Width;
+
+void ScrollText(RGBLedCanvas canvas, RGBLedMatrix matrix)
+{
+    canvas.SetPixels(0, 6, canvas.Width, 6, new Color(0,0,0));
+
+    int pixelsDrawn = canvas.DrawText(font, scrollTextPos, 16, new Color(255, 160, 0), callingPoints);
+
+    scrollTextPos -= 1;
+
+    if (scrollTextPos + pixelsDrawn < 0)
+    {
+        scrollTextPos = canvas.Width;
+    }
+
+    Thread.Sleep(1000);
+}
+
+
 while (running)
 {
-    int postFromEnd = 64 - platformNumber.Count;
+    int postFromEnd = canvas.Width - platformNumber.Count;
+    int timeStartingPos = (canvas.Width - time.Count) / 2;
+
     canvas.DrawText(font, 0, 5, new Color(255, 160, 0), text);
     canvas.DrawText(font, postFromEnd, 5, new Color(255, 160, 0), platformNumber);
     canvas.DrawText(font, 0, 11, new Color(255, 160, 0), destination);
-    canvas.DrawText(font, 0, 16, new Color(255, 160, 0), callingPoints);
-    canvas.DrawText(font, 27, 22, new Color(255, 160, 0), time);
+    ScrollText(canvas, matrix);
+    canvas.DrawText(font, timeStartingPos, 22, new Color(255, 160, 0), time);
     matrix.SwapOnVsync(canvas);
     Thread.Sleep(1000);
     // Thread.Yield();
