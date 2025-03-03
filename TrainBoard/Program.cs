906eb4e509eecedc838e71aed440f3e3c086b4ee
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-03-03 07:05:03 +0000

    added stub data to display

--- TrainBoard/Program.cs
+++ TrainBoard/Program.cs
@@ -1,7 +1,11 @@
 ﻿using RPiRgbLEDMatrix;
 
-string text = "Hello World!";
-
+string std = "15:20";
+string platformNumber = $"Platform {3}";
+string etd = "On Time";
+string destination = "London Liv St.";
+string[] callingPoints = [];
+string time = "15:12:20";
 
 RGBLedMatrixOptions options = new RGBLedMatrixOptions
 {
@@ -17,17 +21,26 @@ using var matrix = new RGBLedMatrix(options);
 var canvas = matrix.CreateOffscreenCanvas();
 using var font = new RGBLedFont("../fonts/4x6.bdf");
 
-canvas.DrawText(font, 0, 6, new Color(255, 160, 0), text);
-canvas.DrawText(font, 0, 12, new Color(255, 160, 0), "Hello lucy");
-matrix.SwapOnVsync(canvas);
-
-// run until user presses Ctrl+C
 var running = true;
 Console.CancelKeyPress += (_, e) =>
 {
     running = false;
-    e.Cancel = true; // do not terminate program with Ctrl+C, we need to dispose
+    e.Cancel = true;
 };
-while (running) Thread.Yield();
+while (running)
+{
+    int postFromEnd = 64 - platformNumber.Count;
+    canvas.DrawText(font, 0, 5, new Color(255, 160, 0), text);
+    canvas.DrawText(font, postFromEnd, 5, new Color(255, 160, 0), platformNumber);
+    canvas.DrawText(font, 0, 11, new Color(255, 160, 0), destination);
+    canvas.DrawText(font, 0, 16, new Color(255, 160, 0), callingPoints);
+    canvas.DrawText(font, 27, 22, new Color(255, 160, 0), time);
+    matrix.SwapOnVsync(canvas);
+    Thread.Sleep(1000);
+    // Thread.Yield();
+}
+
+
+
 
 return 0;
