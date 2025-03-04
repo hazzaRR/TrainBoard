4829d9dc4c23077f61d0b70ce9a93a22022de817
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2025-03-04 09:13:14 +0000

    setup callingpointsWorker to get calling point from cache and display the train services callings point, that scroll across the screen

--- TrainBoard1/Workers/CallingPointsWorker.cs
+++ TrainBoard1/Workers/CallingPointsWorker.cs
@@ -1,3 +1,5 @@
+using System.Buffers;
+using TrainBoard.Entities;
 using TrainBoard.Services;
 
 namespace TrainBoard.Workers;
@@ -6,20 +8,42 @@ public class CallingPointsWorker : BackgroundService
 {
     private readonly ILogger<CallingPointsWorker> _logger;
     private readonly IRgbMatrixService _matrixService;
+    private readonly IMemoryCache _cache;
 
-    public CallingPointsWorker(ILogger<CallingPointsWorker> logger, IRgbMatrixService matrixService)
+
+    public CallingPointsWorker(ILogger<CallingPointsWorker> logger, IRgbMatrixService matrixService, IMemoryCache cache)
     {
         _logger = logger;
         _matrixService = matrixService;
+        _cache = cache;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+
+        int scrollTextPos = _matrixService.Canvas.Width;
+        _cache.TryGetValue("serverHeartbeat", out ScreenData data);
+
         while (!stoppingToken.IsCancellationRequested)
         {
 
             if (_matrixService.IsInitialised)
             {
+
+                _matrixService.Canvas.SetPixels(0, 12, _matrixService.Canvas.Width, 6, new Color(0,0,0));
+
+                int pixelsDrawn = _matrixService.Canvas.DrawText(_matrixService.Font, scrollTextPos, 16, new Color(255, 160, 0), data.CallingPoints);
+
+                scrollTextPos -= 1;
+
+                if (scrollTextPos + pixelsDrawn < 0)
+                {
+                    scrollTextPos = _matrixService.Canvas.Width;
+                    await Task.Delay(2, stoppingToken);
+                    _cache.TryGetValue("serverHeartbeat", out data);
+                }
+
+
 
             }
 
