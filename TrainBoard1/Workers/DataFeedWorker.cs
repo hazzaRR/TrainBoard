43b3145cbfa455b4113107827b38d450879cebe2
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2025-03-04 09:02:21 +0000

    setup entity to store in memory cache

--- TrainBoard1/Workers/DataFeedWorker.cs
+++ TrainBoard1/Workers/DataFeedWorker.cs
@@ -1,6 +1,8 @@
 using TrainBoard.Services;
+using TrainBoard.Entities;
 using OpenLDBWS;
 using OpenLDBWS.Entities;
+using System.Text;
 
 namespace TrainBoard.Workers;
 
@@ -22,23 +24,45 @@ public class DataFeedWorker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
+            LdbwsClient client = new("<api-key>");
 
             if (_matrixService.IsInitialised)
             {
 
+                //read this in from a config file
+
                 string departureStationCode = "COL";
                 string destinationStationCode = "LST";
 
-                LdbwsClient client = new("<api-key>");
 
                 GetDepBoardWithDetailsResponse response = await client.GetDepBoardWithDetails(1, departureStationCode, destinationStationCode);
 
-                //need std, platform and Etd
-                // destination
-                //calling points Name and then std,
+                List<ServiceWithCallingPoints> services = response.StationBoardWithDetails.TrainServices;
 
-                
-                 _cache.Set("departureBoard", response);
+
+                StringBuilder callingPoints = new("Calling at:");
+
+                for (int i = 0; i < services[0].SubsequentCallingPoints.CallingPoints.Count; i++)
+                {
+                    callingPoints.Append($" {services[0].SubsequentCallingPoints.CallingPoints[i].LocationName} ({services[0].SubsequentCallingPoints.CallingPoints[i].St})");
+
+                    if (i < services[0].SubsequentCallingPoints.CallingPoints.Count-1)
+                    {
+                        callingPoints.Append(',');
+                    }
+                }
+
+
+                ScreenData service = new()
+                {
+                    Std = services[0].Std,
+                    Platform = services[0].Platform,
+                    Destination = services[0].Destination[0].LocationName,
+                    CallingPoints = callingPoints.ToString()
+                };
+
+
+                 _cache.Set("departureBoard", service);
 
             }
 
