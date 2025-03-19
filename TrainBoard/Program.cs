1c3380f6f09c7348be7d542834b64e1c44dfb591
hazzarr <harryredman06@gmail.com>
2025-03-19 18:13:19 +0000

    added servies to DI container

--- TrainBoard/Program.cs
+++ TrainBoard/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSingleton<ILdbwsClient>(provider => new LdbwsClient("<Api-ke
 builder.Services.AddHostedService<DataFeedWorker>();
 builder.Services.AddHostedService<PlatformStdToggleWorker>();
 builder.Services.AddHostedService<DisplayWorker>();
+builder.Services.AddHostedService<ScrollCallingPointsWorker>();
 
 var host = builder.Build();
 host.Run();
