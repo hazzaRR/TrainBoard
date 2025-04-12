0742251b56d0ef376014df442fbd8146fa70e475
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2025-04-12 20:38:05 +0100

    added new font to try

--- TrainBoard/Services/CallingPointService/CallingPointService.cs
+++ TrainBoard/Services/CallingPointService/CallingPointService.cs
@@ -3,7 +3,6 @@ namespace TrainBoard.Services;
 public class CallingPointService : ICallingPointService
 {
     public int ScrollTextPos {get; set;}
-
     public int PixelsDrawn {get; set;}
     public bool IsScrollComplete {get; set;} = false;
 }
