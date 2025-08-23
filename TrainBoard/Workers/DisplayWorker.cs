c1e4cf1dec8bad8fe8c3a75feb20256fd18c5f04
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2025-08-23 10:24:07 +0100

    update task delays

--- TrainBoard/Workers/DisplayWorker.cs
+++ TrainBoard/Workers/DisplayWorker.cs
@@ -76,7 +76,7 @@ public class DisplayWorker : BackgroundService
                 _matrixService.Matrix.SwapOnVsync(_matrixService.Canvas);
             }
 
-            await Task.Delay(30, stoppingToken);
+            await Task.Delay(40, stoppingToken);
         }
     }
 
